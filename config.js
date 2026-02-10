e815a27b6ac0a708a15929a0c237a3a912eb6d33
crustykeemchi-67 <crustykeemchi@gmail.com>
2026-02-09 18:29:41 -0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['💛', '❤️', '🤍', '💖', '💓'],  // Heart emojis
+        hearts: ['❤️', '🤍'],  // Heart emojis
         bears: ['🐱', '🌹']                       // Cute bear emojis
     },
 
